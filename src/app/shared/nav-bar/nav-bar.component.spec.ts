7976bff21090979a3cd39dab01a9914231a1a88b
darellanodev <darellano.aliaga@gmail.com>
2025-04-16 22:32:12 +0200

    feat: Create the NavBar component and render the title of the logo (#3)

---
+++ src/app/shared/nav-bar/nav-bar.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NavBarComponent } from './nav-bar.component';
+
+describe('NavBarComponent', () => {
+  let fixture: ComponentFixture<NavBarComponent>;
+  let component: NavBarComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [NavBarComponent],
+    }).compileComponents();
+    fixture = TestBed.createComponent(NavBarComponent);
+    component = fixture.componentInstance;
+    component.logo = 'Stellar Task';
+    fixture.detectChanges();
+  });
+
+  it('should display logo text', () => {
+    const logo = fixture.nativeElement.querySelector('.navbar-brand');
+    expect(logo.textContent).toContain('Stellar Task');
+  });
+});
