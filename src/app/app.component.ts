186b5397c9163f909c0fbaec5b52c9e39487856b
darellanodev <darellano.aliaga@gmail.com>
2025-04-23 14:14:17 +0200

    feat: Display an example task showing its status badge (#4)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { Task } from './task/models/task.model'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core'
 })
 export class AppComponent {
   title = 'stellar-task'
+
+  exampleTask: Task = {
+    id: '1',
+    description: 'Implement TDD approach',
+    createdBy: 'darellanodev',
+    assignedTo: 'darellanodev',
+    requestedBy: 'darellanodev',
+    status: 'pending',
+    createdAt: new Date(),
+  }
 }
