e15a0d6afdcc2bc5873ea9042336afa497ae6b3f
darellanodev <darellano.aliaga@gmail.com>
2025-05-03 21:48:45 +0200

    chore: Improve the karma.conf.js configuration and set the coverage in the angular.json (closes #5)

--- karma.conf.js
+++ karma.conf.js
@@ -1,12 +1,26 @@
 module.exports = function (config) {
   config.set({
-    reporters: ["progress", "coverage"],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    plugins: [
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-coverage"),
+      require("karma-jasmine-html-reporter"),
+      require("@angular-devkit/build-angular/plugins/karma"),
+    ],
+    reporters: ["progress", "kjhtml", "coverage"],
     coverageReporter: {
-      dir: "coverage/",
+      dir: require("path").join(__dirname, "./coverage"),
+      subdir: ".",
       reporters: [
         { type: "html", subdir: "report-html" },
         { type: "lcov", subdir: "report-lcov" },
+        { type: "text-summary" },
       ],
     },
+    browsers: ["Chrome"],
+    singleRun: false,
+    restartOnFileChange: true,
   })
 }
