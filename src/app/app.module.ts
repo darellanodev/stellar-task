bfa9255b425d44a594772d6e8a1e4d568f07b7f9
darellanodev <darellano.aliaga@gmail.com>
2025-05-15 20:15:42 +0200

    feat: Create the day component (#7)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
-import { TaskComponent } from './task/components/task/task.component'
+import { TaskComponent } from './task/components/task/task.component';
+import { DayComponent } from './day/components/day/day.component'
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, TaskComponent],
+  declarations: [AppComponent, NavBarComponent, TaskComponent, DayComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/day/components/day/day.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-day',
+  templateUrl: './day.component.html',
+  styleUrls: ['./day.component.scss'],
+})
+export class DayComponent {}
